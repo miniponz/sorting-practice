8d612b3bf70877269969428a1c221d3e82333ffa
Cara Ponzini <ponzini@gmail.com>
2019-02-07 17:27:21 -0800

    sorted array displays to empty element on button click

--- sorting.js
+++ sorting.js
@@ -1,6 +1,15 @@
 const fruit = ['apples', 'bananas', 'grapes'];
 const veggies = ['cucumber', 'tomato', 'onions'];
 const numbers = [3, 2, 1];
+const sortButton = document.getElementById('sort-button');
+const dataDisplay = document.getElementById('data-display');
+
+dataDisplay.addEventListener('submit', function(event){
+    event.preventDefault();
+    fruit.sort();
+    document.getElementById('fruit').textContent = fruit.join(', ');
+
+});
 
 
 
@@ -8,8 +17,5 @@ const numbers = [3, 2, 1];
 
 
 
-fruit.sort();
-veggies.sort();
-numbers.sort();
 
-console.log(veggies);
+console.log(veggies, fruit, numbers);
