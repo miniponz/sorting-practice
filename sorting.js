7f8cc256ec011df356770c67b8f888ae5aa1b25b
Cara Ponzini <ponzini@gmail.com>
2019-02-07 17:01:42 -0800

    table created and arrays and basic sort function added to js

---
+++ sorting.js
@@ -0,0 +1,15 @@
+const fruit = ['apples', 'bananas', 'grapes'];
+const veggies = ['cucumber', 'tomato', 'onions'];
+const numbers = [3, 2, 1];
+
+
+
+
+
+
+
+fruit.sort();
+veggies.sort();
+numbers.sort();
+
+console.log(veggies);
